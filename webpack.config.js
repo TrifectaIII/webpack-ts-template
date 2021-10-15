ce682bd99d1e874235d225dbaad5a2862fc1b00a
Dakota Madden-Fong <maddenfong@gmail.com>
2021-10-15 11:22:23 -0700

    initial

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+    // mode: 'development',
+    // devtool: 'source-map',
+    mode: 'production',
+    devtool: false,
+    entry: path.resolve(__dirname, './src/index.ts'),
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 8000,
+    },
+
+    module: {
+        rules: [
+            //ts rule
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            //sass rule
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './build'),
+    },
+};
